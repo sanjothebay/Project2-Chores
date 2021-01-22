504e1a8da4f19dc76da8cc47ecf845d142fb88d4
rakeru2006 <rakeru2006@gmail.com>
2021-01-21 23:09:08 -0600

    adding routes

--- server.js
+++ server.js
@@ -16,8 +16,10 @@ app.use(express.json());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+require("./routes/html-routes.js")(app);
 
 // Import routes and give the server access to them.
 /* var routes = require("./controllers/choreController.js");
