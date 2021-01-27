980e80836b015a0aada43aabd6c4c4ac86d47e5d
Luis Diego <chavarria.ld@gmail.com>
2021-01-26 19:19:44 -0600

    adding 3 routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,7 +13,17 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
+
   app.get("/", function(req, res) {
+    res.render("home")
+  })
+  
+  app.get("/input", function(req, res) {
+    res.render("inputForm")
+  })
+
+
+  app.get("/summary", function(req, res) {
     db.Chores.findAll().then(function (data){
       // console.log(data);
       res.render("index", {
@@ -24,8 +34,8 @@ module.exports = function(app) {
 
   });
 
-  app.get("/input", function(req, res) {
-    res.render("inputForm")
-  })
+
+  
+  
 
 };
