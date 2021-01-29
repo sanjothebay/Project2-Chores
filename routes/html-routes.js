ec715b0107b3148cb5a3dc4d746631d20ad81c5b
JC <sanjothebay@gmail.com>
2021-01-28 22:59:49 -0600

    Fixed Users with Diana's help and added buttons

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,45 +8,45 @@ const db = require("../models");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
 
-  app.get("/", function(req, res) {
-    res.render("home")
-  })
-  
-  app.get("/input", function(req, res) {
-    res.render("inputForm")
-  })
+  app.get("/", function (req, res) {
+    res.render("home");
+  });
 
-  
-  app.get('/summary', async function(req, res) {
-    db.Users.findAll().then(function (dataUsers){
-      db.Chores.findAll().then(function (dataChores){
-         res.render("index", {
+  app.get("/input", function (req, res) {
+    res.render("inputForm");
+  });
+
+  app.get("/summary", function (req, res) {
+    db.Chores.findAll({ include: db.Users }).then(function (dataChores) {
+      db.Users.findAll().then(function (dataUsers) {
+        // console.log(dataChores)
+        res.render("index", {
           users: dataUsers,
-           chores: dataChores,
-         })
-         
-     })
-    })
-    })
-  
-  // app.get("/summary", function(req, res) {
-  //   db.Chores.findAll().then(function (data){
-  //      console.log(data);
-  //     res.render("index", {
-  //       //users: data,
-  //       chores: data,
-  //     })
+          chores: dataChores.map((dataChore) => ({
+            ...dataChore,
+            users: dataUsers,
+          })),
+        });
+      });
+    });
+  });
 
+  // app.get('/summary', async function(req, res) {
+  //   db.Users.findAll().then(function (dataUsers){
+  //         console.log(dataUsers);
+  //     db.Chores.findAll().then(function (dataChores){
+  //        console.log(dataChores);
+  //        res.render("index", {
+  //         users: dataUsers,
+  //          chores: dataChores,
+  //        })
+
+  //    })
   //   })
-
-  // })
-  
-  
-
+  //   })
 };
