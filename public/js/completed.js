8456d4093ec4631dc35ee3e3b5caf1ea8e127b6a
JC <sanjothebay@gmail.com>
2021-01-29 21:18:12 -0600

    working on put request

--- public/js/completed.js
+++ public/js/completed.js
@@ -4,13 +4,18 @@ $(function () {
     const userId = $(this).data("user");
     const choreId = $(this).data("chore");
     console.log({ userId, choreId });
-  //   $.ajax("/api/user", {
-  //     type: "POST",
-  //     data: {
-  //         user: userInput
-  //     }
-  // }).then(function(){
-  //     console.log("it worked!");
-  // })
+
+    var gotCleanedUp = {
+      setValue: true
+    };
+
+    $.ajax("/api/chore/", {
+      type: "PUT",
+      data: gotCleanedUp
+          
+      
+  }).then(function(){
+      console.log("it worked!");
+  })
   });
 });
