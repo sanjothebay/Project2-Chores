ba2bd67d20446e8f81f6edf3be8227a8b94367d8
JC <sanjothebay@gmail.com>
2021-01-29 20:23:42 -0600

    working app with button

--- public/js/completed.js
+++ public/js/completed.js
@@ -1,9 +1,16 @@
 $(function () {
-  $(".grid-button").on("click", function (event) {
+  $(".completed-button").on("click", function (event) {
     event.preventDefault();
     const userId = $(this).data("user");
     const choreId = $(this).data("chore");
     console.log({ userId, choreId });
-    
+  //   $.ajax("/api/user", {
+  //     type: "POST",
+  //     data: {
+  //         user: userInput
+  //     }
+  // }).then(function(){
+  //     console.log("it worked!");
+  // })
   });
 });
